03356f4a2fe3ea0391f266b7615b54a538fa25f3
MoHiShazbots <MoHiShazbots1245@gmail.com>
2017-01-23 14:42:31 -0700

    Started Manual Turret stoof

--- src/org/usfirst/frc/team1245/robot/subsystems/Turret.java
+++ src/org/usfirst/frc/team1245/robot/subsystems/Turret.java
@@ -4,20 +4,24 @@ import org.usfirst.frc.team1245.robot.commands.TrackTarget;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Value;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Turret extends Subsystem {
 
-    //Relay rotation, pitch, loader;
-    //Talon shooter;
+    public Relay rotation, pitch;
+    public Victor shooter, loader;
     public UsbCamera cameraRaw = CameraServer.getInstance().startAutomaticCapture("Turret", 0);
     
     public Turret(int rotation, int pitch, int shooter, int loader){
         // TODO Auto-generated method stub
-        //this.rotation = new Relay(rotation);
-        //this.pitch = new Relay(pitch);
-       // this.loader = new Relay(loader);
-        //this.shooter = new Talon(shooter);
+        this.rotation = new Relay(rotation);
+        this.pitch = new Relay(pitch);
+        this.loader = new Victor(loader);
+        this.shooter = new Victor(shooter);
+        this.rotation.set(Value.kOff);
     }
     
     @Override
